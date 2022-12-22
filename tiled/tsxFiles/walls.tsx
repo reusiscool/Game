8ca11f4f6c7c2230b400188871b32c66f85dda9a
reusiscool <robert.prigara@yandex.ru>
2022-12-22 17:51:41 +0300

    tiled update

---
+++ tiled/tsxFiles/walls.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.0" name="walls" tilewidth="200" tileheight="180" tilecount="9" columns="3">
+ <image source="../Images/BigWallTile.png" trans="ffffff" width="600" height="540"/>
+</tileset>
